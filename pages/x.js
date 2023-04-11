c28f6c1aa2f0d3128806c1b33d8d124166605bad
Paul Montgomery <48227329+mulefish@users.noreply.github.com>
2023-04-11 15:16:59 -0700

    Working it

---
+++ pages/x.js
@@ -0,0 +1,232 @@
+const x = {
+    "payload": {
+        "type": "trackEnd",
+        "event": "product-interaction",
+        "properties": {
+            "eventCategoryName": "product-interaction",
+            "urlRoute": "http://localhost:5050/pages/simple.html",
+            "path": "/pages/simple.html",
+            "anonymousId": "eab5542e-d7f7-4d2b-925c-70e618b69439",
+            "language": "en_CA",
+            "browserAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
+            "timestamp": 1681251077402,
+            "property": "web-CA",
+            "country": "CA",
+            "event": {
+                "attributes": {
+                    "action": "view",
+                    "algorithm": "personalized"
+                }
+            },
+            "component": {
+                "id": "recommendation:recently-`viewed",
+                "type": "ProductCarousel",
+                "text": "Buy now",
+                "position": 2,
+                "totalCount": 3
+            },
+            "product": {
+                "skuID": "ca_123",
+                "productID": "prod789",
+                "unifiedID": "",
+                "name": "cool 22\" pants",
+                "localizedName": "",
+                "price": "68.99",
+                "salePrice": "",
+                "isSale": false,
+                "categoryUnifiedID": ""
+            },
+            "collection": {
+                "type": "recommendation",
+                "id": "recently-viewed"
+            },
+            "headerUnified": "simple.html",
+            "pathMutatorObject": {},
+            "validationResult": {
+                "success": true,
+                "data": {
+                    "version": {
+                        "team": "",
+                        "lam": "",
+                        "component": ""
+                    },
+                    "timestamp": 1681251077402,
+                    "payload": {
+                        "collectionList": [{
+                            "productList": [{
+                                "skuList": [{
+                                    "attributes": {
+                                        "errorGuestFacing": false,
+                                        "errorDetails": "",
+                                        "errorType": "",
+                                        "errorMessage": "",
+                                        "orderId": ""
+                                    },
+                                    "price": {
+                                        "saleWithoutTaxShipping": "",
+                                        "regularWithoutTaxShipping": "",
+                                        "taxOnly": "",
+                                        "isSale": false,
+                                        "displaySale": "",
+                                        "displayRegular": ""
+                                    },
+                                    "quantity": -1,
+                                    "size": "",
+                                    "sku": ""
+                                }],
+                                "price": {
+                                    "saleWithoutTaxShipping": "",
+                                    "regularWithoutTaxShipping": "",
+                                    "taxOnly": "",
+                                    "isSale": false,
+                                    "displaySale": "",
+                                    "displayRegular": ""
+                                },
+                                "name": {
+                                    "unified": "",
+                                    "localized": ""
+                                },
+                                "categoryUnifiedId": "",
+                                "unifiedId": "",
+                                "productId": ""
+                            }],
+                            "name": {
+                                "unified": "",
+                                "localized": ""
+                            },
+                            "type": "",
+                            "id": ""
+                        }],
+                        "user": {
+                            "attributes": {
+                                "appId": {
+                                    "sfcc": {
+                                        "id": ""
+                                    },
+                                    "atg": {
+                                        "id": ""
+                                    },
+                                    "fusion": {
+                                        "id": ""
+                                    }
+                                },
+                                "vendors": {
+                                    "google": {
+                                        "googleClickId": ""
+                                    },
+                                    "facebook": {
+                                        "eventId": "",
+                                        "testEventCode": "",
+                                        "fbp": "",
+                                        "fbc": "",
+                                        "fbClickId": ""
+                                    }
+                                },
+                                "genderAffinity": "",
+                                "campaignId": ""
+                            },
+                            "geoIp": {
+                                "zip": "",
+                                "city": "",
+                                "state": "",
+                                "country": "CA",
+                                "ip": ""
+                            },
+                            "browserUserAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
+                            "profile": {
+                                "membershipType": "unknown",
+                                "isLoggedIn": false,
+                                "hashedEmail": ""
+                            },
+                            "anonymousId": "eab5542e-d7f7-4d2b-925c-70e618b69439"
+                        },
+                        "event": {
+                            "component": {
+                                "attributes": {
+                                    "errorGuestFacing": false,
+                                    "errorDetails": "",
+                                    "errorType": "",
+                                    "errorMessage": "",
+                                    "orderId": ""
+                                },
+                                "internalCampaignId": {
+                                    "workstreamSpecified": "",
+                                    "workstreamBase": "",
+                                    "type": "",
+                                    "testVariation": "",
+                                    "testName": "",
+                                    "row": -1,
+                                    "businessInitiative": "",
+                                    "pageName": {
+                                        "name": {
+                                            "unified": "",
+                                            "localized": ""
+                                        }
+                                    },
+                                    "ctaPageName": {
+                                        "name": {
+                                            "unified": "",
+                                            "localized": ""
+                                        }
+                                    }
+                                },
+                                "placement": {
+                                    "totalCount": 3,
+                                    "position": 2
+                                },
+                                "text": "Buy now",
+                                "type": "ProductCarousel",
+                                "id": "recommendation:recently-`viewed"
+                            },
+                            "attributes": {
+                                "errorGuestFacing": false,
+                                "errorDetails": "",
+                                "errorType": "",
+                                "errorMessage": "",
+                                "orderId": ""
+                            },
+                            "eventSubType": "",
+                            "type": "click-tap",
+                            "id": "recommendation:recently-`viewed"
+                        },
+                        "screen": {
+                            "attributes": {},
+                            "category": {
+                                "gender": "",
+                                "hierarchy": [{
+                                    "name": {
+                                        "unified": "",
+                                        "localized": ""
+                                    }
+                                }]
+                            },
+                            "header": {
+                                "unified": "simple.html",
+                                "localized": ""
+                            },
+                            "language": "en_CA",
+                            "property": "web-CA",
+                            "currency": "",
+                            "collections": [""],
+                            "country": "CA",
+                            "type": "",
+                            "path": "/pages/simple.html",
+                            "urlRoute": "http://localhost:5050/pages/simple.html"
+                        }
+                    }
+                }
+            }
+        },
+        "options": {},
+        "userId": "eab5542e-d7f7-4d2b-925c-70e618b69439",
+        "anonymousId": "59cf6883-5eba-4810-9eb5-b14232432f8b",
+        "meta": {
+            "rid": "d246b139-e36e-4ec0-be70-3577fbfb42c8",
+            "ts": 1681251077402,
+            "hasCallback": true
+        }
+    }
+}
+
+
+console.log( x['payload']['properties']['validationResult']['data']['payload'] ) 
